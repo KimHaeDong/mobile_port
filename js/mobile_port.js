1d43781dc511ecf77ad38f478bdf99a79e52b9a7
KimHaeDong <68522342+KimHaeDong@users.noreply.github.com>
2020-11-04 17:04:06 +0900

    Add files via upload

--- js/mobile_port.js
+++ js/mobile_port.js
@@ -1,32 +1,49 @@
 (function ($) {
 
-    $("#wrap").on(
-        "click",
-        "#header h1 a, .conTit prev a",
-        function () {
-          var url = this.href;
-          $("#container > #content").remove();
-          $("#container").load(url + " #content");
-          return false;
-        }
-      );
+  $("#wrap").on(
+    "click",
+    "#header h1 a, .conTit prev a",
+    function () {
+      var url = this.href;
+      $("#container > #content").remove();
+      $("#container").load(url + " #content");
+      return false;
+    }
+  );
 
-    $("body").on("click", "#step_area .setArea a, .mountainContent .mountainiList a, .menuBox a", function (e) {
-    e.preventDefault();
-    var url = this.href;
-    var part = $(this).attr('class')
-    $("#container > #content").remove();
-    $("#container").load(url + " #content", function () {
-      var newContent = "";
-      for (var i in usedata[part]) {
-        newContent += `<li><div class="img"><img src="${usedata[part][i].photo}" alt=""></div>`;
-        newContent += `<div class="doctorInfo"><strong>${usedata[part][i].name}</strong>`;
-        newContent += `<p>${usedata[part][i].depart}</p>`;
-        newContent += `<div>${usedata[part][i].about}</div></div></li>`;
+  var usedata;
+  $.ajax({
+    type: "GET",
+    url: "data/mountain.json",
+    beforeSend: function (xhr) {
+      if (xhr.overrideMimeType) {
+        xhr.overrideMimeType("application/json");
       }
-      $("#content .part1DoctorList").html(`<ul>${newContent}</ul>`);
-      });
+    },
+    success: function (data) {
+      usedata = data;
+    },
+    error: function (abc) {
+      alert(abc.status + "오류발생");
+    },
+  })
+
+  $("body").on("click", "#step_area .setArea a, .mountainContent .mountainiList a, .menuBox a", function (e) {
+  e.preventDefault();
+  var url = this.href;
+  var part = $(this).attr('class')
+  $("#container > #content").remove();
+  $("#container").load(url + " #content", function () {
+    var newContent = "";
+    for (var i in usedata[part]) {
+      newContent += `<li><div class="img"><img src="${usedata[part][i].photo}" alt=""></div>`;
+      newContent += `<div class="doctorInfo"><strong>${usedata[part][i].name}</strong>`;
+      newContent += `<p>${usedata[part][i].depart}</p>`;
+      newContent += `<div>${usedata[part][i].about}</div></div></li>`;
+    }
+    $("#content .part1DoctorList").html(`<ul>${newContent}</ul>`);
     });
+  });
 
   $("#topIcon .menu").on("click", function () {
     $("#navWrap").addClass("on");
